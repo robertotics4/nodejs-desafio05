c64812bbc800160e9630c6d9ce79143e5903d8af
robertotics4 <robertooliveiragp@gmail.com>
2020-11-05 23:27:59 -0300

    :sparkles: challenge concluded

--- src/routes/transaction.routes.ts
+++ src/routes/transaction.routes.ts
@@ -1,15 +1,17 @@
 import { Router } from 'express';
 
-// import TransactionsRepository from '../repositories/TransactionsRepository';
-// import CreateTransactionService from '../services/CreateTransactionService';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
 
 const transactionRouter = Router();
-
-// const transactionsRepository = new TransactionsRepository();
+const transactionsRepository = new TransactionsRepository();
 
 transactionRouter.get('/', (request, response) => {
   try {
-    // TODO
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+
+    return response.json({ transactions, balance });
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
@@ -17,7 +19,21 @@ transactionRouter.get('/', (request, response) => {
 
 transactionRouter.post('/', (request, response) => {
   try {
-    // TODO
+    const { title, value, type } = request.body;
+
+    const typeLowerCase = type.toLowerCase();
+
+    const createTransaction = new CreateTransactionService(
+      transactionsRepository,
+    );
+
+    const transaction = createTransaction.execute({
+      title,
+      value,
+      type: typeLowerCase,
+    });
+
+    return response.status(201).json(transaction);
   } catch (err) {
     return response.status(400).json({ error: err.message });
   }
